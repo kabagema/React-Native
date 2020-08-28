3af8095e863c75e4b43da7d020d98a3447fdafe6
kabagema <35719135+kabagema@users.noreply.github.com>
2020-08-28 10:55:30 +0200

    Revert "commit where?"
    This reverts commit cee75a118a61c92261489ca98a5c71b996c3eb41.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import React, {useState} from 'react';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Header from './Header';
-import Nav from './Nav';
-import Results from './Results';
-import requests from "./requests"
 
 function App() {
-  const [selectedOption, setSelectedOption] = useState
-  (requests.fetchTrending);
-  
   return (
-    <div className = "app">
-
-      <Header />
-      <Nav setSelectedOption={setSelectedOption}/>
-      <Results selectedOption={selectedOption}/>
-
-      {/*Header*/}
-      {/* Nav*/}
-      {/*Results*/}
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-    
-    
-
   );
 }
 
