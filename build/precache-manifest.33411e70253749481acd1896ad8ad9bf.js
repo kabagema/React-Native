ce11e19c8fea468410ee2c2015b4f66ecce812ed
kabagema <35719135+kabagema@users.noreply.github.com>
2020-08-28 10:57:50 +0200

    hulu app

---
+++ build/precache-manifest.33411e70253749481acd1896ad8ad9bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "672daed3cb0fef2fe4ad859c9ea35ba8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8671030d41b844e56882",
+    "url": "/static/css/main.4ae0c4e6.chunk.css"
+  },
+  {
+    "revision": "efd2d71dcb9ec4ba79d1",
+    "url": "/static/js/2.025314e8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.025314e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8671030d41b844e56882",
+    "url": "/static/js/main.2c70b866.chunk.js"
+  },
+  {
+    "revision": "e23f96d817477a4cb65a",
+    "url": "/static/js/runtime-main.268a3519.js"
+  }
+]);
